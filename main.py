e8c8319815550d85bdb4739d707707ca5d219205
CarlsonCarlson <CarlsonCarlson@github.com>
2021-03-12 18:25:40 -0500

    initializing repo

---
+++ main.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def main(name):
+    x1 = np.arange(0, 2 * (np.pi), 0.1)
+    y1 = np.sin(x1)
+
+    x2 = np.arange(0, 2 * (np.pi), 0.1)
+    y2 = np.cos(x2)
+
+    plt.plot(x1, y1)
+    plt.show()
+
+    plt.plot(x2, y2)
+    plt.show()
+
+
+if __name__ == '__main__':
+    main('PyCharm')
