e33ed5600c09404a30ed638c7618f6b5d5a6e112
CarlsonCarlson <carlson.cheng888@gmail.com>
2021-04-27 15:41:40 -0400

    class today (4/21/21) + get_info function

--- signal_plot.py
+++ signal_plot.py
@@ -2,44 +2,47 @@ import matplotlib.pyplot as plot
 import numpy as np
 from scipy.io import wavfile
 import wave
+from scipy.fft import fft, fftfreq
+
+
+def get_info(filename: str) -> dict:
+    sound = wave.open(filename, "r")
+    # Don't know what this is but it said 2
+    # print("sample width in bytes:")
+    # print(sound.getsampwidth())
+    # Duration in seconds
+    duration = sound.getnframes() / sound.getsampwidth()
+    info_dict = {
+        # 1 is mono 2 is stereo
+        "num_channels": sound.getnchannels(),
+        "sampling_freq": sound.getframerate(),
+        "total_signals": sound.getnframes(),
+        "duration": duration
+    }
+    return info_dict
 
 
 def main():
-    sound = wave.open("Bird Call.wav", "r")
-    sampling_rate = 48000
-    Tg = 1000000000 / sampling_rate
-
+    filename = "Bird Call.wav"
+    info_dict = get_info(filename)
     # NOTES: Geophysical time is real time Tg (22 secs)
     # Arrival time
-
-    # Find what stuff does
-    print("get n channels (1 is mono 2 is stereo:")
-    print(sound.getnchannels())
-    print("sample width in bytes:")
-    print(sound.getsampwidth())
-    print("sampling frequency:")
-    print(sound.getframerate())
-    print("number of audio frames:")
-    print(sound.getnframes())
 
     # Used this website
     # https://pythontic.com/visualization/signals/spectrogram
 
     # Using scipy package instead of wave
-    # sampling_frequency, signal_data = wavfile.read('Bird Call.wav')  # creates a vector with the frequency in 0,
+    sampling_frequency, signal_data = wavfile.read('Bird Call.wav')  # creates a vector with the frequency in 0,
     # sampling_frequency, signal_data = wavfile.read('Whistling_tones.wav')
-    sampling_frequency, signal_data = wavfile.read('high_low_screaming.wav')
+    # sampling_frequency, signal_data = wavfile.read('high_low_screaming.wav')
     # and the array of signals in [1]
-
-    print(sampling_frequency)
-    print(signal_data)
 
     # Plot the signal read from wav file
 
     plot.subplot(211)  # tall enough for 2, wide enough for 1, plot #1
-    # plot.title("Spectrogram of a Bird's Call")
+    plot.title("Spectrogram of a Bird's Call")
     # plot.title("Spectrogram of Whistling Tones")
-    plot.title("Spectrogram of Carlson Screaming")
+    # plot.title("Spectrogram of Carlson Screaming")
     plot.plot(signal_data)
     plot.xlabel('Sample')
     plot.ylabel('Amplitude')
@@ -80,10 +83,35 @@ def main():
     # TODO: learn about fourier transforms
     # https://www.youtube.com/watch?v=spUNpyF58BY
 
-    # TODO: make fast fourier transform of my 3 signals, not spectrogram
+    print(info_dict["total_signals"])
+    number_of_samples = info_dict["total_signals"]
+    # fftfreq() calculates the frequencies in the center of each bin in the output of fft(). Without this,
+    # there would be no way to plot the x-axis on your frequency spectrum.
+    x_fourier = fftfreq(number_of_samples, 1 / sampling_frequency)
+    y_fourier = fft(signal_data)
+    plot.plot(np.abs(x_fourier), np.abs(y_fourier))
+    # // rounds the divided number to the nearest whole number
+    plot.xlabel("Frequency")
+    plot.ylabel("Amplitude")
+    plot.show()
 
     # TODO: make a clap signal and look at it in the fourier transform graph, not the spectrogram graph
     # TODO: find what is power spectral density with the fourier transform
+
+    # Taking out the motorcycle
+    # TODO: make vector with length equal to total number of signals
+    # TODO: make index 0-2000 in the vector equal to 0, and the rest 1s
+    # TODO: multiply the signal data by the vector
+    # TODO: use inverse fft on the resultant vector
+    # TODO: turn the new signal data into a new cleaned up wav file
+
+    # Decibels are
+    # reference pressure is usually 20 micropascal/Hz in air
+    # the reference pressure(p0) is divided by freq so that the pressure is standard at all frequency
+    # Decibels = log10(pressure / reference pressure)
+
+    # Convolution or match filter
+    # replica correlation
 
 
 if __name__ == '__main__':
