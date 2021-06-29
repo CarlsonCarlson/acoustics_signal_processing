87414d53751d0cfaa7e39c664a98d11cede7af8c
CarlsonCarlson <carlson.cheng888@gmail.com>
2021-06-29 12:44:38 -0400

    final commit, filtering doesn't work

--- signal_plot.py
+++ signal_plot.py
@@ -1,8 +1,10 @@
 import matplotlib.pyplot as plot
 import numpy as np
+import scipy.io.wavfile
 from scipy.io import wavfile
 import wave
-from scipy.fft import fft, fftfreq
+from scipy.fft import fft, fftfreq, ifft
+
 
 # To convert the file from m4a to wav I use the audioconverter package
 # Sample command line
@@ -48,15 +50,12 @@ def main():
     plot.subplot(211)  # tall enough for 2, wide enough for 1, plot #1
     plot.title("Spectrogram of " + filename.replace(".wav", ""))
     plot.plot(signal_data)
-    # TODO: figure out how to zoom in and out of all graphs
-    # using linspace an xticks to scale the axis to the time domain
-    # plot.xticks(np.linspace())
-    plot.xlabel('Sample')
+
+    plot.xlabel('Time (seconds)')
     plot.ylabel('Amplitude')
     print(info_dict["total_signals"])
     print(info_dict["sampling_freq"])
     print(info_dict["duration"])
-    print(signal_data)
 
     plot.subplot(212)  # tall enough for 2, wide enough for 1, plot #2
     spectrogram = plot.specgram(signal_data, Fs=sampling_frequency)
@@ -92,9 +91,6 @@ def main():
 
     # More than one frequency is a broadband signal while a single frequency is just one signal
 
-    # TODO: learn about fourier transforms
-    # https://www.youtube.com/watch?v=spUNpyF58BY
-
     print(info_dict["total_signals"])
     number_of_samples = info_dict["total_signals"]
     # fftfreq() calculates the frequencies in the center of each bin in the output of fft(). Without this,
@@ -104,33 +100,74 @@ def main():
     plot.plot(np.abs(x_fourier), np.abs(y_fourier))
     # // rounds the divided number to the nearest whole number
     plot.xlabel("Frequency (Hz)")
-    plot.ylabel("Amplitude")
+    plot.ylabel("Amplitude^2")
     plot.show()
 
-    # TODO: make a clap signal and look at it in the fourier transform graph, not the spectrogram graph
-    # TODO: find what is power spectral density with the fourier transform
-
+    # the fourier transform is nothing but an integral of the frequency domain
+    # the result of a fourier transform is the power spectral density with axes frequency vs amplitude^2
+    # power spectral density is a function that tells you how much energy is in the signal
+    # then by squaring the amplitude of the normal amplitude vs time signal data you can combine it with the PSD
+    # a spectrogram is a 3d function equal time, frequency, energy (amplitude^2)
+    #
     # The power spectral density (PSD) of the signal describes the power present in the signal as a function of
     # frequency, per unit frequency. Power spectral density is commonly expressed in watts per hertz (W/Hz).
 
     # Taking out the motorcycle sound
-    # TODO: make vector with length equal to total number of signals
-    # TODO: make index 0-2000 in the vector equal to 0, and the rest 1s
-    # TODO: multiply the signal data by the vector
-    # TODO: use inverse fft on the resultant vector
-    # TODO: turn the new signal data into a new cleaned up wav file
+    # this makes a (10,0) array of zeroes
+    # zeros_array = np.zeros(10)
+    # print(zeros_array)
+    # print(zeros_array.shape)
+
+    # this creates an array of 1s that is (5,10)
+    # so a list of 5 elements, each element is a list of 10 1s
+    # example_ones_array = np.ones(5,10)
+    # print(example_ones_array)
+    # print(example_ones_array.shape)
+
+    # ones_array = np.ones(5)
+    # print("")
+    # combined_array = np.concatenate((zeros_array, ones_array))
+    # print(combined_array)
+
+    filter_array = np.ones(info_dict["total_signals"])
+    np.put(filter_array, np.arange(0, 2000), [0])
+    cleaned_data = y_fourier * filter_array
+    print(cleaned_data)
+
+    plot.plot(np.abs(x_fourier), np.abs(cleaned_data))
+    # // rounds the divided number to the nearest whole number
+    plot.xlabel("Frequency (Hz)")
+    plot.ylabel("Amplitude^2")
+    plot.show()
+
+    # changing back to time domain so we can write the wav file
+    time_cleaned_data = ifft(cleaned_data)
+    print(time_cleaned_data)
+
+    plot.specgram(signal_data, Fs=sampling_frequency)
+    plot.xlabel('Time (s)')
+    plot.ylabel('Frequency (Hz)')
+    plot.ylim(0, 5000)  # Make frequency range from 0-5000khz
+    # colormap = plot.get_cmap("viridis")
+    # colormap.set_under(color='k', alpha=None)
+    # plot.colorbar(spectrogram)
+    plot.show()
+
+    converted_time_cleaned_data = cleaned_data.astype(dtype="float32")
+    scipy.io.wavfile.write("filtered_audio.wav", info_dict["sampling_freq"], converted_time_cleaned_data)
+    # that did not work, but I'm not sure why
 
     # Decibels are
     # reference pressure is usually 20 micropascal/Hz in air
     # the reference pressure(p0) is divided by freq so that the pressure is standard at all frequency
-    # Decibels = log10(pressure / reference pressure)
+    # Decibels = 10 * log10(pressure / reference pressure)
+    # pressure^2 = intensity
+    # Decibel is 20 * log10(pressure)
 
     # Convolution or match filter
     # replica correlation
     # multiplying the sound by itself
 
-    # TODO: make the fourier transform run in real time
-    # TODO: Take in real time sound and output real time graph
 
     # Resonance
     # When frequencies resonate, the amplitudes reach their highest signal to noise ratio
@@ -138,9 +175,12 @@ def main():
     # Every object has multiple resonance frequencies called modes
     # 3 standing waves mean its at the mode 3 frequency
     # Helmholtz resonator whirrs when you blow into the opening from a certain angle
-
+    #
     # 3 mediums in sound
     # source (generator), medium (propagation), receiver
+    #
+    # cos function is just a normal
+    # integrating from 0 to 2 pi of cos is equal to pressure^2 which equals intensity
 
 
 if __name__ == '__main__':
